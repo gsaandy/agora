c7e6b187aaae35358d13d536cde9642d6f604628
Gibu George <gibu.george@nibodha.com>
2015-12-01 06:58:46 +0530

    Changed from application.properties to application.ml for configuration. Added logback.xml and imx config for logs Secured the admin ui

--- launcher/src/main/java/com/nibodha/ip/launcher/PlatformLauncher.java
+++ launcher/src/main/java/com/nibodha/ip/launcher/PlatformLauncher.java
@@ -5,12 +5,16 @@ package com.nibodha.ip.launcher;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by gibugeorge on 28/11/15.
  */
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
 @EnableAdminServer
 public class PlatformLauncher {
 
