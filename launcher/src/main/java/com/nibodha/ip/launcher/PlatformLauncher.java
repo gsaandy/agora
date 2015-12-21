24b7ce495d6b5bffd1c551eb0254bb914109e37e
Gibu George <gibu.george@nibodha.com>
2015-12-21 06:42:42 +0530

    Adding platform support to customise jetty server

--- launcher/src/main/java/com/nibodha/ip/launcher/PlatformLauncher.java
+++ launcher/src/main/java/com/nibodha/ip/launcher/PlatformLauncher.java
@@ -5,15 +5,23 @@ package com.nibodha.ip.launcher;
 
 import com.nibodha.ip.camel.RouteDefinitionsInjector;
 import com.nibodha.ip.config.PlatformConfiguration;
+import org.eclipse.jetty.server.NetworkTrafficServerConnector;
+import org.eclipse.jetty.server.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.jetty.JettyServerCustomizer;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.List;
 
 /**
  * Created by gibugeorge on 28/11/15.
@@ -27,7 +35,7 @@ public class PlatformLauncher extends SpringBootServletInitializer {
         new PlatformLauncher().run(args);
 
     }
-
+åå
     public void run(final String[] args) {
         final SpringApplication application = new SpringApplication(PlatformLauncher.class);
         application.setRegisterShutdownHook(true);
@@ -36,6 +44,15 @@ public class PlatformLauncher extends SpringBootServletInitializer {
         final ConfigurableApplicationContext configurableApplicationContext = application.run(args);
         final RouteDefinitionsInjector routeDefinitionsInjector = configurableApplicationContext.getBean(RouteDefinitionsInjector.class);
         routeDefinitionsInjector.inject();
+    }
+
+    @Bean
+    public JettyEmbeddedServletContainerFactory jettyEmbeddedServletContainerFactory(
+            @Value("${server.port:8080}") final String mainPort) {
+
+        final JettyEmbeddedServletContainerFactory factory = new JettyEmbeddedServletContainerFactory(Integer.valueOf(mainPort));
+
+        return factory;
     }
 
 
