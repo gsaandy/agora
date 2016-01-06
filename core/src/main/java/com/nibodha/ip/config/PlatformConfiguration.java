7e2d3b88fbc8dcbdb96bb58a5317e287143cbc7e
Abhilash Ramachandran <abhilash.r@nibodha.com>
2016-01-06 10:52:53 +0530

    Disabling Spring Boot automatic configuration for Datasource

--- core/src/main/java/com/nibodha/ip/config/PlatformConfiguration.java
+++ core/src/main/java/com/nibodha/ip/config/PlatformConfiguration.java
@@ -6,12 +6,14 @@ package com.nibodha.ip.config;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.PropertyPlaceholderAutoConfiguration;
 import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @ImportResource("classpath*:META-INF/spring/nip-application-context.xml")
-@EnableAutoConfiguration(exclude = {PropertyPlaceholderAutoConfiguration.class, BatchAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {PropertyPlaceholderAutoConfiguration.class, BatchAutoConfiguration.class, DataSourceAutoConfiguration.class,DataSourceTransactionManagerAutoConfiguration.class})
 public class PlatformConfiguration {
 }
